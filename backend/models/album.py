9ad9d0c9b76b1deae1a7b46db4a1cdad6bba02c3
bugemarvin <bugemarvin@outlook.com>
2024-11-30 18:05:51 +0300

    Added Models - User model - album model - photo model

---
+++ backend/models/album.py
@@ -0,0 +1,7 @@
+from models import db
+
+class Album(db.Model):
+    __tablename__ = 'albums'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
